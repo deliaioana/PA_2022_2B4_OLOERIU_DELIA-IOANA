dba0382a59bb0175da8fd7409430f4193dd587ca
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-05-29 15:11:34 +0300

    untracked files

---
+++ sgbd_apis/src/main/java/group/sgbd_apis/SgbdApisApplication.java
@@ -0,0 +1,13 @@
+package group.sgbd_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgbdApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgbdApisApplication.class, args);
+	}
+
+}
---
+++ sgbd_apis/src/test/java/group/sgbd_apis/SgbdApisApplicationTests.java
@@ -0,0 +1,13 @@
+package group.sgbd_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgbdApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
